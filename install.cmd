a49c0b1bd5295d923fac113af6048c55effaa886
Abakumov <Denis.Abakumov@investorsgroup.com>
2022-03-29 10:02:13 -0500

    (un-)install scripts added

---
+++ install.cmd
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe %~dp0\RegistryValueMonitorService\bin\Release\RegistryValueMonitorService.exe
+pause
---
+++ uninstall.cmd
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe /u %~dp0\RegistryValueMonitorService\bin\Release\RegistryValueMonitorService.exe
+pause
